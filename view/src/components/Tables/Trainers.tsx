43ea47e636a4d66976dad2f5034153048bf50bda
adityakcnit <adityakcnit@gmail.com>
2025-03-26 23:10:19 +0530

    Create the new Trainer table

---
+++ view/src/components/Tables/Trainers.tsx
@@ -0,0 +1,141 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../redux/store';
+import { useEffect, useState } from 'react';
+import { deleteTrainer, getAllTrainers } from '../../redux/features/auth/trainerSlice';
+import Loader from '../../common/Loader';
+import { MdDelete, MdFileCopy, MdModeEdit } from 'react-icons/md';
+import { getTokenData, isAdmin } from '../../utils/Utils';
+import { toast } from 'react-toastify';
+import DeleteModal from '../Modal/DeleteModal';
+
+const Trainers = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const trainers = useSelector((state: RootState) => state.trainer.trainersList);
+  const admin = isAdmin();
+
+  const [selectedTrainerId, setSelectedTrainerId] = useState<string>('');
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const tokenData = getTokenData();
+  const loggedInTrainerId = tokenData?.id;
+
+  const handleDelete = (trainerId: string) => {
+    setSelectedTrainerId(trainerId);
+    setShowDeleteModal(true);
+  };
+
+  const confirmDelete = async () => {
+    try {
+      const data = await dispatch(deleteTrainer(selectedTrainerId)).unwrap();
+      toast.success(data?.message || 'Trainer deleted successfully');
+      setShowDeleteModal(false);
+      setSelectedTrainerId('');
+      dispatch(getAllTrainers());
+    } catch (error) {
+      toast.error(error as string);
+    }
+  };
+
+  useEffect(() => {
+    dispatch(getAllTrainers()); // Dispatch the API call when component mounts
+  }, [dispatch]); // Runs only on mount
+
+  if (trainers.loading) return <Loader />;
+
+  if (trainers.error) {
+    toast.dismiss(); // dismiss previous toasts
+    toast.error(trainers.error, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      theme: 'light',
+    });
+  }
+
+  return (
+    <div className="rounded-sm border border-stroke bg-white px-5 pt-6 pb-2.5 shadow-default dark:border-strokedark dark:bg-boxdark sm:px-7.5 xl:pb-1">
+      <div className="max-w-full overflow-x-auto">
+        <table className="w-full table-auto">
+          <thead>
+            <tr className="bg-gray-2 text-left dark:bg-meta-4">
+              {admin && <th className="py-4 px-4 font-medium text-black dark:text-white">Actions</th>}
+              <th className="min-w-[220px] py-4 px-4 font-medium text-black dark:text-white xl:pl-11">Name</th>
+              <th className="min-w-[150px] py-4 px-4 font-medium text-black dark:text-white">Email</th>
+              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">Phone Number</th>
+              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">Created On</th>
+              <th className="min-w-[120px] py-4 px-4 font-medium text-black dark:text-white">Changed On</th>
+            </tr>
+          </thead>
+          <tbody>
+            {trainers.data?.map((trainer, key) => (
+              <tr key={key}>
+                {admin && (
+                  <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <div className="flex items-center space-x-3.5">
+                      <button className="hover:text-primary">
+                        <MdModeEdit size={22} />
+                      </button>
+                      <button
+                        className="hover:text-primary"
+                        onClick={() => {
+                          navigator.clipboard.writeText(trainer._id);
+                        }}
+                      >
+                        <MdFileCopy size={22} />
+                      </button>
+                      {trainer._id !== loggedInTrainerId && (
+                        <button className="text-danger hover:text-red-600" onClick={() => handleDelete(trainer._id)}>
+                          <MdDelete size={22} />
+                        </button>
+                      )}
+                    </div>
+                  </td>
+                )}
+                <td className="border-b border-[#eee] py-5 px-4 pl-9 dark:border-strokedark xl:pl-11">
+                  <h5 className="font-medium text-black dark:text-white">{trainer.name}</h5>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <p className="text-black dark:text-white">{trainer.email}</p>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <p className="text-black dark:text-white">{trainer.phone_no}</p>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <p className="text-black dark:text-white">
+                    {new Date(trainer.createdAt).toLocaleString('en-US', {
+                      month: 'numeric',
+                      day: 'numeric',
+                      year: '2-digit',
+                      hour: 'numeric',
+                      minute: '2-digit',
+                      hour12: true,
+                    })}
+                  </p>
+                </td>
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                  <p className="text-black dark:text-white">
+                    {new Date(trainer.updatedAt).toLocaleString('en-US', {
+                      month: 'numeric',
+                      day: 'numeric',
+                      year: '2-digit',
+                      hour: 'numeric',
+                      minute: '2-digit',
+                      hour12: true,
+                    })}
+                  </p>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <DeleteModal
+          entityType="trainer"
+          open={showDeleteModal}
+          onClose={() => setShowDeleteModal(false)}
+          onConfirm={() => confirmDelete()}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Trainers;
