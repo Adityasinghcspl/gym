2fe8294888de95f44e9fc5052800194fab73a997
adityakcnit <adityakcnit@gmail.com>
2025-03-26 23:07:59 +0530

    Add the toast into the app.tsx file

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
 
 import SiteRoutes from './routes/SiteRoutes';
 import DashboardRoutes from './routes/DashboardRoutes';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
-  const [loading, setLoading] = useState<boolean>(false);
-  const { pathname } = useLocation();
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const { pathname } = useLocation();
 
   // useEffect(() => {
   //   window.scrollTo(0, 0);
@@ -18,10 +19,22 @@ function App() {
 
   // Check if running on dashboard subdomain
   const isDashboard = window.location.hostname === 'dashboard.localhost';
-
+  const isDarkMode = document.documentElement.classList.contains('dark');
   // return loading ? (
   // <Loader />
-  return isDashboard ? <DashboardRoutes /> : <SiteRoutes />;
+  return (
+    <>
+      {isDashboard ? <DashboardRoutes /> : <SiteRoutes />}
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        theme={isDarkMode ? 'dark' : 'light'}
+      />
+    </>
+  );
 }
 
 export default App;
