1f2acfcca39cfa82d7857662422aa831742fb800
adityakcnit <adityakcnit@gmail.com>
2025-04-18 22:24:41 +0530

    remove commented code

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,27 +1,12 @@
-// import { useEffect, useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-
 import SiteRoutes from './routes/SiteRoutes';
 import DashboardRoutes from './routes/DashboardRoutes';
 import { ToastContainer } from 'react-toastify';
 
 function App() {
-  // const [loading, setLoading] = useState<boolean>(false);
-  // const { pathname } = useLocation();
-
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, [pathname]);
-
-  // useEffect(() => {
-  //   setTimeout(() => setLoading(false), 1000);
-  // }, []);
-
   // Check if running on dashboard subdomain
   const isDashboard = window.location.hostname === 'dashboard.localhost';
   const isDarkMode = document.documentElement.classList.contains('dark');
-  // return loading ? (
-  // <Loader />
+
   return (
     <>
       {isDashboard ? <DashboardRoutes /> : <SiteRoutes />}
