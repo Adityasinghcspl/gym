36e543586155ed884b3aade7b27dfd8ffd08de80
adityakcnit <adityakcnit@gmail.com>
2025-04-19 15:55:52 +0530

    update the breadcrumb

--- view/src/components/Breadcrumbs/Breadcrumb.tsx
+++ view/src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,27 +1,29 @@
 import { Link } from 'react-router-dom';
 interface BreadcrumbProps {
   pageName: string;
-  leftElement?: React.ReactNode;
+  RightElement?: React.ReactNode;
 }
-const Breadcrumb = ({ pageName, leftElement }: BreadcrumbProps) => {
+const Breadcrumb = ({ pageName, RightElement }: BreadcrumbProps) => {
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-      {!leftElement && <h2 className="text-title-md2 font-semibold text-black dark:text-white">{pageName}</h2>}
+      <h2 className="text-title-md2 font-semibold text-black dark:text-white">{pageName}</h2>
 
-      {leftElement}
-
-      <div className="flex items-center gap-4">
-        <nav>
-          <ol className="flex items-center gap-2">
-            <li>
-              <Link className="font-medium" to="/">
-                Dashboard /
-              </Link>
-            </li>
-            <li className="font-medium text-primary">{pageName}</li>
-          </ol>
-        </nav>
-      </div>
+      {RightElement ? (
+        RightElement
+      ) : (
+        <div className="flex items-center gap-4">
+          <nav>
+            <ol className="flex items-center gap-2">
+              <li>
+                <Link className="font-medium" to="/">
+                  Dashboard /
+                </Link>
+              </li>
+              <li className="font-medium text-primary">{pageName}</li>
+            </ol>
+          </nav>
+        </div>
+      )}
     </div>
   );
 };
