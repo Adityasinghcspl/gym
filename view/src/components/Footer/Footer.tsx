54be781b118ac1406b302bacedaa84f8db11e9c3
adityakcnit <adityakcnit@gmail.com>
2025-04-24 23:57:49 +0530

    Update the footer style and link to add

--- view/src/components/Footer/Footer.tsx
+++ view/src/components/Footer/Footer.tsx
@@ -1,36 +1,44 @@
+import { useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { FaFacebookF, FaTwitter, FaYoutube, FaInstagram, FaEnvelope } from 'react-icons/fa';
 
 export default function Footer() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
   return (
     <section className="bg-black-2 pt-12">
       <div className="container mx-auto px-4 lg:px-16 xl:px-24">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
           {/* About Section */}
-          <div className="sm:col-span-2 md:col-span-3 lg:col-span-1">
+          <div className="sm:col-span-2 md:col-span-3 lg:col-span-2">
             <div className="mb-6">
-              <a href="#">
+              <Link to="/">
                 <img src="/img/logo.png" alt="Logo" className="mb-6" />
-              </a>
+              </Link>
               <p className="text-gray-400 leading-6">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
                 dolore magna aliqua.
               </p>
             </div>
-            <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 text-lg">
-                <i className="fa fa-facebook"></i>
-              </a>
-              <a href="#" className="text-gray-400 text-lg">
-                <i className="fa fa-twitter"></i>
-              </a>
-              <a href="#" className="text-gray-400 text-lg">
-                <i className="fa fa-youtube-play"></i>
-              </a>
-              <a href="#" className="text-gray-400 text-lg">
-                <i className="fa fa-instagram"></i>
-              </a>
-              <a href="#" className="text-gray-400 text-lg">
-                <i className="fa fa-envelope-o"></i>
-              </a>
+            <div className="flex space-x-4 text-gray-400 text-xl">
+              <Link to="#">
+                <FaFacebookF />
+              </Link>
+              <Link to="#">
+                <FaTwitter />
+              </Link>
+              <Link to="#">
+                <FaYoutube />
+              </Link>
+              <Link to="#">
+                <FaInstagram />
+              </Link>
+              <Link to="#">
+                <FaEnvelope />
+              </Link>
             </div>
           </div>
 
@@ -39,45 +47,45 @@ export default function Footer() {
             <h4 className="text-white font-semibold mb-4">Useful Links</h4>
             <ul className="text-gray-400 space-y-2">
               <li>
-                <a href="#">About</a>
+                <Link to="/about-us">About</Link>
               </li>
               <li>
-                <a href="#">Blog</a>
+                <Link to="/services">Service</Link>
               </li>
               <li>
-                <a href="#">Classes</a>
+                <Link to="/our-team">Our Teams</Link>
               </li>
               <li>
-                <a href="#">Contact</a>
+                <Link to="/contact-us">Contact</Link>
               </li>
             </ul>
           </div>
 
           {/* Support */}
-          <div className="sm:col-span-1 md:col-span-1 lg:col-span-1">
+          {/* <div className="sm:col-span-1 md:col-span-1 lg:col-span-1">
             <h4 className="text-white font-semibold mb-4">Support</h4>
             <ul className="text-gray-400 space-y-2">
               <li>
-                <a href="#">Login</a>
+                <Link to="#">Login</Link>
               </li>
               <li>
-                <a href="#">My Account</a>
+                <Link to="#">My Account</Link>
               </li>
               <li>
-                <a href="#">Subscribe</a>
+                <Link to="#">Subscribe</Link>
               </li>
               <li>
-                <a href="#">Contact</a>
+                <Link to="#">Contact</Link>
               </li>
             </ul>
-          </div>
+          </div> */}
 
           {/* Tips & Guides */}
           <div className="sm:col-span-2 md:col-span-1 lg:col-span-2">
             <h4 className="text-white font-semibold mb-4">Tips & Guides</h4>
             <div className="border-b border-gray-700 pb-4 mb-4">
               <h6 className="text-gray-400">
-                <a href="#">Physical fitness may help prevent depression, anxiety</a>
+                <Link to="#">Physical fitness may help prevent depression, anxiety</Link>
               </h6>
               <ul className="text-sm text-gray-500 flex space-x-4">
                 <li>3 min read</li>
@@ -86,7 +94,7 @@ export default function Footer() {
             </div>
             <div>
               <h6 className="text-gray-400">
-                <a href="#">Fitness: The best exercise to lose belly fat and tone up...</a>
+                <Link to="#">Fitness: The best exercise to lose belly fat and tone up...</Link>
               </h6>
               <ul className="text-sm text-gray-500 flex space-x-4">
                 <li>3 min read</li>
@@ -97,9 +105,7 @@ export default function Footer() {
         </div>
 
         <div className="text-center border-t border-gray-700 mt-8 py-6">
-          <p className="text-gray-400 text-sm">
-            Copyright &copy; {new Date().getFullYear()} All rights reserved 
-          </p>
+          <p className="text-gray-400 text-sm">Copyright &copy; {new Date().getFullYear()} All rights reserved</p>
         </div>
       </div>
     </section>
